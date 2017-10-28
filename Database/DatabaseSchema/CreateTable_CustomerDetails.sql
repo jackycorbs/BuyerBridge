9604b6ba20877580fdaca75ccdebef3e253a87c7
Ryan <ryan.eyrex@gmail.com>
2017-10-28 16:11:06 +0100

    Added Database schema

---
+++ Database/DatabaseSchema/CreateTable_CustomerDetails.sql
@@ -0,0 +1,14 @@
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[CustomerDetails]') AND type in (N'U'))
+
+BEGIN
+CREATE TABLE [dbo].[CustomerDetails](
+	CUST_ID UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	CUST_FirstName VARCHAR(20) NOT NULL,
+	CUST_LastName VARCHAR(20) NOT NULL,
+	CUST_OtherNames VARCHAR(20) NULL,
+	CUST_Telephone1 VARCHAR(30) NULL,
+	CUST_Telephone2 VARCHAR(20) NULL
+) 
+
+END
